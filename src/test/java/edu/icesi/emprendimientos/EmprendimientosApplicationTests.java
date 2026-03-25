d1633f4af3c15f1b06d1fa5118be777d80f9f7ab
Ximena <ximenagg2002@gmail.com>
2026-03-25 17:49:22 -0500

    Initial Spring Boot project

---
+++ src/test/java/edu/icesi/emprendimientos/EmprendimientosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.icesi.emprendimientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendimientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
