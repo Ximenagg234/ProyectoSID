3d93c048c202e3e896d80b5658a67073a0d286ae
Natalia Delgado <nataliadelgadoarenas18@gmail.com>
2026-05-31 10:53:44 -0500

    security config modificado

--- src/main/java/edu/icesi/emprendimientos/security/SecurityConfig.java
+++ src/main/java/edu/icesi/emprendimientos/security/SecurityConfig.java
@@ -75,6 +75,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/ws/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class)
